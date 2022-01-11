656582647e9ab583f7fb993a89deae165852fadc
kay-stump04 <stumpkay000@ephrataschools.org>
2022-01-11 16:02:20 +0000

    year of the tiger

---
+++ studentWork/kay-stump04/GitHubCodespacesTest/CodespacesBeta.java
@@ -0,0 +1,9 @@
+
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "2022 is the Year of the TIGER!";
+        System.out.println(x);
+        
+    }
+}
