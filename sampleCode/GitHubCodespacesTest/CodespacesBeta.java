cbbcfbbc91c362db3c17a85580abe54ea516e4b0
kay-stump04 <stumpkay000@ephrataschools.org>
2022-01-28 16:38:49 +0000

    h

--- sampleCode/GitHubCodespacesTest/CodespacesBeta.java
+++
@@ -1,9 +0,0 @@
-package sampleCode.GitHubCodespacesTest;
-
-public class CodespacesBeta {
-    public static void main(String[] args) {
-        String x = "Welcome Back !!!";
-        System.out.println(x);
-        
-    }
-}
--- sampleCode/Hello/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package sampleCode.Hello;
-
-public class HelloWorld {
-    public static void main (String [] args) {
-        System.out.println("HelloWorld");
-    }
-}
